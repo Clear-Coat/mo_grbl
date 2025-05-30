30e92dbd5ec0a08302cf6b89a0532e94b694203c
Noah Gaeta <noah@moutfitters.com>
2025-05-30 12:45:44 -0400

    fix

--- grbl/main.c
+++ grbl/main.c
@@ -38,14 +38,14 @@ volatile uint8_t sys_rt_exec_accessory_override; // Global realtime executor bit
 
 int main(void)
 {
- // ? Set pin 7 to output
-  DDRD |= (1 << DDD4);
-  // ? Set pin 7 to LOW
-  PORTD &= ~(1 << PORTD4);
-  // ? wait 5ms
-  delay_ms(5);
-  // ? Set pin 7 to HIGH
-  PORTD |= (1 << PORTD4);
+//  // ? Set pin 7 to output
+//   DDRD |= (1 << DDD4);
+//   // ? Set pin 7 to LOW
+//   PORTD &= ~(1 << PORTD4);
+//   // ? wait 5ms
+//   delay_ms(5);
+//   // ? Set pin 7 to HIGH
+//   PORTD |= (1 << PORTD4);
   
   // Initialize system upon power-up.
   serial_init();   // Setup serial baud rate and interrupts
