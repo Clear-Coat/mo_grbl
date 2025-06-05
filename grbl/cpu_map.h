01ff85c80d1df34a4f9fe9332a0f865596dbb0a4
Noah Gaeta <noah@moutfitters.com>
2025-06-05 15:12:23 -0400

    try this 2

--- grbl/cpu_map.h
+++ grbl/cpu_map.h
@@ -47,7 +47,7 @@
   #define X_DIRECTION_BIT   5  // Uno Digital Pin 5
   #define Y_DIRECTION_BIT   6  // Uno Digital Pin 6
   #define Z_DIRECTION_BIT   7  // Uno Digital Pin 7
-  #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)|(1<<Z_DIRECTION_BIT)) // All direction bits
+  #define DIRECTION_MASK    ((1<<X_DIRECTION_BIT)|(1<<Y_DIRECTION_BIT)) // All direction bits
 
   // Define stepper driver enable/disable output pin.
   #define STEPPERS_DISABLE_DDR    DDRB
--- grbl/vcm.c
+++ grbl/vcm.c
@@ -8,6 +8,6 @@ void vcm_init()
         DDRD |= (1 << DDD7);
         // Set pin 7 to HIGH
         // PORTD |= (1 << PORTD7);
-        PORTD &= ~(1 << PORTD7);
+        PORTD |= 1 << PORTD7;
     }
 }
